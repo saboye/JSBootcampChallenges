559917f7b2280eb23a74c3cdc7ccc6d8e81614b6
SAMUEL ABOYE <saboyecode@gmail.com>
2023-03-19 22:37:34 -0500

    add function and string solution

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,21 +1,55 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
 
+// solution 1
+// function max(num1, num2, num3) {
+//   if (num1 >= num2 && num1 >= num3) {
+//     return num1;
+//   } else if (num2 >= num1 && num2 >= num3) {
+//     return num2;
+//   } else {
+//     return num3;
+//   }
+// }
+
+// solution 2
+function max(num1, num2, num3) {
+  return Math.max(num1, num2, num3);
 }
 
 function addUnknown() {
-
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
 }
 
-function operator() {
-
+function operator(num1, num2, num3) {
+  if (num1 + num2 === num3) {
+    return "plus";
+  } else if (num1 - num2 === num3) {
+    return "minus";
+  } else if (num1 * num2 === num3) {
+    return "multiply";
+  } else if (num1 / num2 === num3) {
+    return "divide";
+  } else {
+    return null;
+  }
 }
 
 module.exports = {
-  max, addUnknown, evenOrOdd, operator,
+  max,
+  addUnknown,
+  evenOrOdd,
+  operator,
 };
--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,17 +1,26 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  return str.includes(letter);
 }
 
 function cap(str, letter) {
-
+  const index = str.indexOf(letter.toLowerCase());
+  if (index === -1 || index === str.length - 1) {
+    return "sorry not found";
+  }
+  const nextLetter = str[index + 1];
+  return nextLetter.toUpperCase();
 }
 
 function firstCharacter(str1, str2) {
-
+  const strOne = str1[0];
+  const strTwo = str2[0];
+  return strOne.toLowerCase() === strTwo.toLowerCase() || strOne === strTwo;
 }
 
 module.exports = {
-  cap, firstCharacter, doesInclude,
+  cap,
+  firstCharacter,
+  doesInclude,
 };
