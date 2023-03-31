0ea46fdd0b676c2cdd41cabd4a14ae989ae17f70
SAMUEL ABOYE <saboyecode@gmail.com>
2023-03-31 00:23:40 -0500

    Add arrayMethod code and Readme

--- tests/06_arrayMethods/arrayMethods.js
+++ tests/06_arrayMethods/arrayMethods.js
@@ -1,33 +1,50 @@
-function doubler() {
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 
+
+
+function doubler(arr) {
+  return arr.map((num) => num * 2);
 }
 
-function mapCap() {
-
+function mapCap(arr) {
+  return arr.map((str) => str.toUpperCase());
 }
 
-function canRideTheRide() {
-
+function canRideTheRide(riders) {
+  return riders.map((rider) => {
+    if (rider.age >= 17) {
+      return `${rider.name} can ride the ride`;
+    } else {
+      return `Sorry, ${rider.name} cannot ride the ride`;
+    }
+  });
 }
 
-function indAndVal() {
-
+function indAndVal(arr) {
+  return arr.map((value, index) => ({ index, value }));
 }
 
-function filterEvens() {
-
+function filterEvens(arr) {
+  return arr.filter((num) => num % 2 === 0);
 }
 
-function gmailSearch() {
 
+function gmailSearch(emails) {
+  return emails.filter((email) => email.includes("@gmail.com"));
 }
 
-function getCart() {
 
+function getCart(arr) {
+  const cartTotal = arr
+    .filter((item) => item.price < 100)
+    .reduce((total, item) => total + item.price, 0);
+  return cartTotal;
 }
 
-function findGreater() {
-
+function findGreater(cart) {
+  return cart.find((item) => item.price > 100);
 }
 
 module.exports = {
